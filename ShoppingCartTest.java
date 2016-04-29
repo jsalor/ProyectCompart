3b0e4862de3f7d8d5bd99e2da38edab99d6cc755
jsalor <jorgesalor24@gmail.com>
2016-04-29 10:19:30 +0200

    Creado el test

---
+++ ShoppingCartTest.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShoppingCartTest {
+	
+	
+	
+
+	@Test
+	public void testShoppingCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItemCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+}
